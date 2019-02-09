183af2ef55ad20991f0663bb90de8dcf19249b66
raimop <ray@tlu.ee>
2019-02-09 17:41:28 +0200

    1.3.1

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ else {
 	$defaultChecked = $checkedCode;
 }
 
-/* ainenimetused */
+/* Ainenimetused */
 
 /* Ainulaadsed ained esmaspäev */
 
@@ -54,33 +54,36 @@ $subject_tarkvaraarenduse_praktika = 'Tarkvaraarenduse praktika';
 
 /* Ainulaadsed ained reedel */
 
-$subject_robootika = '09:00 - Roobotika';
+$subject_robootika = '09:00 - Robootika';
 
 
 /* lingid */
 
 /* Ainulaadsed lingid esmaspäev */
 
-$link_eesrakenduste_arendamine = '<a href="#" class="under">Eesrakenduste</a>';
-$link_programmeerimise_pohikursus = '<a href="#" class="under">Java</a>';
-$link_arvutigraafika = '<a href="#" class="under">Arvutigraafika</a>';
-$link_tarkvara_testimise_alused = '<a href="#" class="under">Testimise</a>';
-$link_algoritmid_ja_andmestruktuurid = '<a href="#" class="under">Algoritmid</a>';
+$link_eesrakenduste_arendamine = '<a href="https://github.com/eesrakenduste-arendamine-2019" class="under" target="_blank" >github</a>';
+$link_programmeerimise_pohikursusI = '<a href="https://github.com/mrxkollo/IFI6069.DT-Programmeerimise-pohikursus-2019" class="under" target="_blank">github</a>';
+$link_programmeerimise_pohikursusII_1 = '<a href="https://github.com/prpohi19/" class="under" target="_blank">github</a><br>';
+$link_programmeerimise_pohikursusII_2 = '<a href="http://minitorn.tlu.ee/~jaagup/kool/java/kursused/19/prpohi/juht.html" class="under" target="_blank">info</a>';
+$link_arvutigraafika = '<a href="https://drive.google.com/drive/folders/16nmXG5iH356qCkUHbTHk7XNwyDenwoGB" class="under" target="_blank">drive</a>';
+$link_tarkvara_testimise_alused = '<a href="https://drive.google.com/drive/folders/1_I0WtP9xsxnRBql6L0kDw8ECniI-Ci5N" class="under" target="_blank">drive</a>';
+$link_algoritmid_ja_andmestruktuurid = '<a href="http://www.cs.tlu.ee/~inga/AAS/" class="under" target="_blank">info</a>';
 
 /* Ainulaadsed lingid teisipäev */
 
-$link_hulgateooria_ja_loogika_elemendid = '<a href="#" class="under">Hulgateooria</a>';
-$link_yldotstarbelised_arendusplatvormid = '<a href="#" class="under">link</a>';
+$link_hulgateooria_ja_loogika_elemendid = '<a href="https://ois2.tlu.ee/tluois/!uus_ois2.ois_public.page?_page=4A012E8D2A0B1470E4011CBEF33BC141153B8AEB279EABF7&p_id=42329" class="under" target="_blank">ois2</a>';
+$link_yldotstarbelised_arendusplatvormid = '<a href="http://minitorn.tlu.ee/~jaagup/kool/java/kursused/19/yldplatvormid/juht.html" class="under" target="_blank">info</a>';
 
 /* Ainulaadsed ained kolmapäeval */
 /* Ainulaadsed ained neljapäeval */
 
-$link_veeb_ja_meedia_elemendid = '<a href="#" class="under">Veeb</a>';
-$link_tarkvaraarenduse_praktika = '<a href="#" class="under">Tarkvaraarenduse</a>';
+$link_veeb_ja_meedia_elemendid1 = '<a href="https://greeny.cs.tlu.ee/~rinde/2019/veeb_ja_meedia_dm " class="under" target="_blank">greeny</a><br>';
+$link_veeb_ja_meedia_elemendid2 = '<a href="https://drive.google.com/drive/folders/1cFx1giEwKgH_vO29W4Wi9HjF9r9XKa_g" class="under" target="_blank">drive</a>';
+$link_tarkvaraarenduse_praktika = '<a href="http://www.cs.tlu.ee/~inga/TTP/" class="under" target="_blank">info</a>';
 
 /* Ainulaadsed ained reedel */
 
-$link_robootika = '<a href="#" class="under">Roobotika</a>';
+$link_robootika = '<a href="https://www.tlu.ee/~aido/robootika/" class="under" target="_blank">Robootika</a>';
 
 
 ?>
@@ -107,7 +110,9 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		echo "a.special:hover { color: white; }\n";
 		echo ".more a.under:hover { color: white; border-color: hotpink; }\n";
 		echo ".handdrawnbox { border: 2px solid #ffffff; }\n";
-		echo ".label { color: white; }\n";	
+		echo ".label { color: white; }\n";
+		echo ".lights { background-color: black; color: white; }\n";
+		echo ".panel { background: #000; }\n";
 		
 	} 	
 	
@@ -165,12 +170,13 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 
 <div class="container">
 <div class="ribbon bottom-right posiFixed hotpink"><a href="changelog.log" class="specialTwo" target="_blank">changelog</a></div>
-<h5>1.3.0</h5>
+<h5>1.3.1</h5>
 <h4>IF18</h4>
 <div class="tabs">
   <input class="input" name="tabs" type="radio" id="tab-1"<?php echo $firstChecked . $defaultChecked?>/>
   <label class="label" for="tab-1">TA I</label>
   <div class="panel">
+	<div class="lights">theme: <a href="?tab=i">☼ light</a> <a href="?tab=i&theme=dark">☾ dark</a></div>
     <table class="table">
 	<tr class="weekdays">
 		<td></td>
@@ -190,7 +196,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 	</tr>
 	<tr>
 		<th>10:15 - 11:45</th>
-		<td class="handdrawnbox Monday" rowspan="2"><a id="hide12" href="#hide12" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show12" href="#show12" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursus ?></div></td>
+		<td class="handdrawnbox Monday" rowspan="2"><a id="hide12" href="#hide12" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show12" href="#show12" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursusI ?></div></td>
 		<td class="handdrawnbox Tuesday"><a id="hide13" href="#hide13" class="hide"><?php echo $subject_hulgateooria_ja_loogika_elemendid ?></a><a id="show13" href="#show13" class="show"><?php echo $subject_hulgateooria_ja_loogika_elemendid ?></a><div class="more"><?php echo $link_hulgateooria_ja_loogika_elemendid ?></div></td>
 		<td></td>
 		<td></td>
@@ -216,7 +222,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		<th>16:15 - 17:45</th>
 		<td class="handdrawnbox Monday"><a id="hide18" href="#hide18" class="hide"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><a id="show18" href="#show18" class="show"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><div class="more"><?php echo $link_algoritmid_ja_andmestruktuurid ?></div></td>
 		<td class="handdrawnbox Tuesday" rowspan="2"><a id="hide19" href="#hide19" class="hide"><?php echo $subject_eesrakenduste_arendamine ?></a><a id="show19" href="#show19" class="show"><?php echo $subject_eesrakenduste_arendamine ?></a><div class="more"><?php echo $link_eesrakenduste_arendamine ?></div></td>
-		<td class="handdrawnbox Wednesday"><a id="hide110" href="#hide110" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show110" href="#show110" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
+		<td></td>
 		<td class="handdrawnbox Thursday"><a id="hide111" href="#hide111" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show111" href="#show111" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
 		<td></td>
 	</tr>
@@ -233,6 +239,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
   <input class="input" name="tabs" type="radio" id="tab-2"<?php echo $secondChecked?>/>
   <label class="label" for="tab-2">TA II</label>
   <div class="panel">
+	<div class="lights">theme: <a href="?tab=ii">☼ light</a> <a href="?tab=ii&theme=dark">☾ dark</a></div>
     <table class="table">
 	<tr class="weekdays">
 		<td></td>
@@ -262,7 +269,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		<th>12:15 - 13:45</th>
 		<!-- <dt></dt> -->
 		<td></td>
-		<td class="handdrawnbox Wednesday" rowspan="2"><a id="hide26" href="#hide26" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show26" href="#show26" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursus ?></div></td>
+		<td class="handdrawnbox Wednesday" rowspan="2"><a id="hide26" href="#hide26" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show26" href="#show26" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursusII_1 . $link_programmeerimise_pohikursusII_2 ?></div></td>
 		<!--<td></td>-->
 		<td></td>
 	</tr>
@@ -278,7 +285,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		<th>16:15 - 17:45</th>
 		<td class="handdrawnbox Monday"><a id="hide29" href="#hide29" class="hide"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><a id="show29" href="#show29" class="show"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><div class="more"><?php echo $link_algoritmid_ja_andmestruktuurid ?></div></td>
 		<td></td>
-		<td class="handdrawnbox Wednesday"><a id="hide210" href="#hide210" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show210" href="#show210" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
+		<td></td>
 		<td class="handdrawnbox Thursday"><a id="hide211" href="#hide211" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show211" href="#show211" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
 		<td></td>
 	</tr>
@@ -295,6 +302,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
   <input class="input" name="tabs" type="radio" id="tab-3"<?php echo $thirdChecked?>/>
   <label class="label" for="tab-3">DM</label>
   <div class="panel">
+	<div class="lights">theme: <a href="?tab=dm">☼ light</a> <a href="?tab=dm&theme=dark">☾ dark</a></div>
     <table class="table">
 	<tr class="weekdays">
 		<td></td>
@@ -316,8 +324,8 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		<th>10:15 - 11:45</th>
 		<td></td>
 		<td class="handdrawnbox Tuesday"><a id="hide32" href="#hide32" class="hide"><?php echo $subject_hulgateooria_ja_loogika_elemendid ?></a><a id="show32" href="#show32" class="show"><?php echo $subject_hulgateooria_ja_loogika_elemendid ?></a><div class="more"><?php echo $link_hulgateooria_ja_loogika_elemendid ?></div></td>
-		<td class="handdrawnbox Wednesday" rowspan="2"><a id="hide33" href="#hide33" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show33" href="#show33" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursus ?></div></td>
-		<td class="handdrawnbox Thursday" rowspan="2"><a id="hide34" href="#hide34" class="hide"><?php echo $subject_veeb_ja_meedia_elemendid ?></a><a id="show34" href="#show34" class="show"><?php echo $subject_veeb_ja_meedia_elemendid ?></a><div class="more"><?php echo $link_veeb_ja_meedia_elemendid ?></div></td>
+		<td class="handdrawnbox Wednesday" rowspan="2"><a id="hide33" href="#hide33" class="hide"><?php echo $subject_programmeerimise_pohikursus ?></a><a id="show33" href="#show33" class="show"><?php echo $subject_programmeerimise_pohikursus ?></a><div class="more"><?php echo $link_programmeerimise_pohikursusI ?></div></td>
+		<td class="handdrawnbox Thursday" rowspan="2"><a id="hide34" href="#hide34" class="hide"><?php echo $subject_veeb_ja_meedia_elemendid ?></a><a id="show34" href="#show34" class="show"><?php echo $subject_veeb_ja_meedia_elemendid ?></a><div class="more"><?php echo $link_veeb_ja_meedia_elemendid1 . $link_veeb_ja_meedia_elemendid2 ?></div></td>
 		<!--<td></td>-->
 	</tr>
 	<tr>
@@ -340,7 +348,7 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 		<th>16:15 - 17:45</th>
 		<td class="handdrawnbox Monday"><a id="hide39" href="#hide39" class="hide"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><a id="show39" href="#show39" class="show"><?php echo $subject_algoritmid_ja_andmestruktuurid ?></a><div class="more"><?php echo $link_algoritmid_ja_andmestruktuurid ?></div></td>
 		<td></td>
-		<td class="handdrawnbox Wednesday"><a id="hide310" href="#hide310" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show310" href="#show310" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
+		<td></td>
 		<td class="handdrawnbox Thursday"><a id="hide311" href="#hide311" class="hide"><?php echo $subject_tarkvaraarenduse_praktika ?></a><a id="show311" href="#show311" class="show"><?php echo $subject_tarkvaraarenduse_praktika ?></a><div class="more"><?php echo $link_tarkvaraarenduse_praktika ?></div></td>
 		<td></td>
 	</tr>
@@ -355,17 +363,17 @@ if ($numberOfTheDay == 1 or $numberOfTheDay == 2 or $numberOfTheDay == 3 or $num
 </table>
   </div>
 </div>
-<h2>¹ ligipääsetav ainult läbi TLÜ lan-ist vöi tunneliga</h2>
-<h2>² no https</h2>
+<h2>¹ ligipääsetav ainult TLÜ LAN-is või tunnelis</h2>
+<h2>² https (valikuline) ainult TLÜ LAN-is/tunnelis</h2>
 <?php
 
 if ($numberOfTheDay > 5) {
-	echo "<h3>** No highlight due to it being ";
+	echo "<h3>** Esiletõst puudub, kuna täna on ";
 	
 	if ($numberOfTheDay == 6 ) {
-		echo "Saturday</h3>";	
+		echo "laupäev</h3>";	
 	} elseif ($numberOfTheDay == 7) {
-		echo "Sunday</h3>";
+		echo "pühapäev</h3>";
 	} elseif ($numberOfTheDay > 7) {
 		echo "Alien Day (please contact Fox Molder and Dana Scully) no, seriously, please report this bug, no such day (php date('N') > 7)</h3>";
 	}
@@ -374,19 +382,49 @@ if ($numberOfTheDay > 5) {
 
 <h4>Üldinfo</h4>
 <ul>
-	<li><h8><a href="https://discord.gg/pTMsdgF" class="special" target="_blank">DISCORDI server</a></h8> (Slacki enam ei kasuta, see on selle asemel =) )</li>
+	<li>
+		<h8><a href="https://discord.gg/pTMsdgF" class="special" target="_blank">DISCORDI server</a></h8> (Slacki enam ei kasuta, see on selle asemel =) )
+	</li>
 	<li>student:D..</li>
 	<li>\\bitt\if18 | if18:ra..</li>
 	<li>greeny.cs.tlu.ee | osa_eesnimest + osa_perenimest:pw</li>
 	<li>lin2.tlu.ee | ÕIS kasutaja:pw</li>
-	<li><a href="https://www.tlu.ee/asio/" class="special" target="_blank">ASIO</a> (https) | <a href="https://www.tlu.ee/asio/kalenterit2/index.php?av_v=&av=180910180916180916&cluokka=&kt=lk&laji=&guest=intranet%2Ftu&lang=est&ui=&yks=&clk%5B%5D=IFIFB-1&jagu=4&b_lk=Esita+valik+kombineeritult+%3E%3E&b=1537092414&av_y=0" class="special" target="_blank">ASIO IFIF18 1.õ.-a.</a> (https)</li>
-	<li><a href="https://www.tlu.ee/masio/" class="special" target="_blank">mASIO</a> (https) | <a href="https://www.tlu.ee/masio/?id=ryhm&ryhm=IFIFB-1#mASIO" class="special" target="_blank">mASIO IFIF18 1.õ.-a.</a> (https)</li>
-	<li><a href="https://ois2.tlu.ee/" class="special" target="_blank">ÕIS</a> (https)</li>
-	<li><a id="hide40" href="#hide40" class="hide special">+ GitHub käsud (push)</a><a id="show40" href="#show40" class="show special">- GitHub käsud (push)</a><div class="more"><p>git add .</p><p>git commit -m "kommentaar"</p><p>git push</p></div></li>
-	<li><a id="hide41" href="#hide41" class="hide special">+ SQL info</a><a id="show41" href="#show41" class="show special">- SQL info</a><div class="more"><p><a href="http://greeny.cs.tlu.ee/phpMyAdmin" class="special" target="_blank">greeny.cs.tlu.ee/phpMyAdmin</a> (tunneliga ainult)</p><p>greeny (putty): mysql -uif.. -pif.. if18_nimi (spetsiaalselt ei kirjutanud välja)</p></div></li>
-	<li><a id="hide42" href="#hide42" class="hide special">+ muud materjalid</a><a id="show42" href="#show42" class="show special">- muud materjalid</a><div class="more"><p><a href="http://caupo.ee/tlu/" class="special" target="_blank">http://caupo.ee/tlu/</a></p><p><a href="http://www.cs.tlu.ee/instituut/nouded/lopu_too/yliopilastoode_vormistusjuhend_2016a.pdf" class="special" target="_blank">Lõputöö vormistamise juhend</a></p><p><a href="https://madis0.gitlab.io/tlu-eap/" class="special" target="_blank">Madise EAP calc</a></p><p><a href="https://drive.google.com/drive/u/0/folders/16hDQYkxrAxfkkIYyaWXUOwvxAp4VpnH1" class="special" target="_blank">Caupo TLÜ Google Drive Shared</a></p><p><a href="https://products.office.com/en/student/office-in-education" class="special" target="_blank">Office 365 for students for free</a></p></div></li>
+	<li>
+		<a href="https://www.tlu.ee/asio/" class="special" target="_blank">ASIO</a> (https) | <a href="https://www.tlu.ee/asio/kalenterit2/index.php?cluokka=IFIFB-1&kt=lk&guest=intranet%2Ftu&lang=est" class="special" target="_blank">ASIO IFIF18 1.õ.-a.</a> (https)
+	</li>
+	<li>
+		<a href="https://www.tlu.ee/masio/" class="special" target="_blank">mASIO</a> (https) | <a href="https://www.tlu.ee/masio/?id=ryhm&ryhm=IFIFB-1#mASIO" class="special" target="_blank">mASIO IFIF18 1.õ.-a.</a> (https)
+	</li>
+	<li>
+		<a href="https://ois2.tlu.ee/" class="special" target="_blank">ÕIS</a> (https)
+	</li>
+	<li>
+		<a id="hide40" href="#hide40" class="hide special">+ GitHub käsud (push)</a><a id="show40" href="#show40" class="show special">- GitHub käsud (push)</a>
+		<div class="more">
+			<p>git add .</p>
+			<p>git commit -m "kommentaar"</p>
+			<p>git push</p>
+		</div>
+	</li>
+	<li>
+		<a id="hide41" href="#hide41" class="hide special">+ SQL info</a><a id="show41" href="#show41" class="show special">- SQL info</a><div class="more">
+			<p><a href="http://greeny.cs.tlu.ee/phpMyAdmin" class="special" target="_blank">greeny.cs.tlu.ee/phpMyAdmin</a> (ainult tunneliga)</p>
+			<p>greeny (putty): mysql -uif.. -pif.. if18_nimi (spetsiaalselt ei kirjutanud välja)</p>
+		</div>
+	</li>
+	<li>
+		<a id="hide42" href="#hide42" class="hide special">+ muud materjalid</a><a id="show42" href="#show42" class="show special">- muud materjalid</a>
+		<div class="more">
+			<p><a href="http://caupo.ee/tlu/" class="special" target="_blank">http://caupo.ee/tlu/</a></p>
+			<p><a href="http://www.cs.tlu.ee/instituut/nouded/lopu_too/yliopilastoode_vormistusjuhend_2016a.pdf" class="special" target="_blank">Lõputöö vormistamise juhend</a></p>
+			<p><a href="https://madis0.gitlab.io/tlu-eap/" class="special" target="_blank">Madise EAP calc</a></p>
+			<p><a href="https://drive.google.com/drive/u/0/folders/16hDQYkxrAxfkkIYyaWXUOwvxAp4VpnH1" class="special" target="_blank">Caupo TLÜ Google Drive Shared</a></p>
+			<p><a href="https://products.office.com/en/student/office-in-education" class="special" target="_blank">Office 365 õpilastele (piiratud)</a></p>
+			<p><a href="https://e5.onthehub.com/WebStore/ProductsByMajorVersionList.aspx?ws=c5b379d9-989b-e011-969d-0030487d8897" class="special" target="_blank">Microsoft Imagine veebipood</a></p>
+		</div>
+	</li>
 </ul>
-<div class="by">by Raimo Pregel, 2019</div>
+<div class="by">Raimo Pregel, 2019</div>
 </div>
 
 </body>
